4d808c0cc8816629912d71b3e7399fcc7b06de63
Joshua Wootonn <joshuawootonn@gmail.com>
2018-06-16 16:09:26 -0500

    discoverModelsFromDb.js works on an array.👌

--- api/server/scripts/discoverModelsFromDB.js
+++ api/server/scripts/discoverModelsFromDB.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const writeFile = promisify(fs.writeFile);
 const readFile = promisify(fs.readFile);
 const mkdirp = promisify(require('mkdirp'));
+const _ = require('lodash');
 
 const DATASOURCE_NAME = 'db';
 const dataSourceConfig = require('../datasources.json');
@@ -20,40 +21,44 @@ async function discover() {
   // It's important to pass the same "options" object to all calls
   // of dataSource.discoverSchemas(), it allows the method to cache
   // discovered related models
-  const options = {relations: true};
+  const options = { relations: true };
 
   // Discover models and relations
-  const authoritySchemas = await db.discoverSchemas('authority', options);
-  const crewSchemas = await db.discoverSchemas('crew', options);
-  const employeeSchemas = await db.discoverSchemas('EMPLOYEE', options);
-  const shiftSchemas = await db.discoverSchemas('SHIFT', options);
-  const activitySchemas = await db.discoverSchemas('ACTIVITY', options);
-  const projectTaskSchemas = await db.discoverSchemas('PROJECT_TASK', options);
-  const projectSchemas = await db.discoverSchemas('PROJECT', options);
-  const taskSchemas = await db.discoverSchemas('TASK', options);
-  const subcategorySchemas = await db.discoverSchemas('SUBCATEGORY', options);
-  const categorySchemas = await db.discoverSchemas('CATEGORY', options);
-  const dimensionSchemas = await db.discoverSchemas('DIMENSION', options);
+
+  const modelNames = ['authority', 'crew','employee','shift','activity','project_task', 'project', 'task', 'subcategory','category', 'dimension'];
+
+  let schemas = [];
+
+
+  for (let modelName of modelNames) {
+    schemas.push(await db.discoverSchemas(modelName, options))
+  }
 
   // Create model definition files
   await mkdirp('../../common/models');
-  await writeFile(
-    '../../common/models/authority.json',
-    JSON.stringify(authoritySchemas['time-track-development.authority'], null, 2)    
-  );
-  await writeFile(
-    '../../common/models/crew.json',
-    JSON.stringify(crewSchemas['time-track-development.crew'], null, 2)    
-  );
-  
+ 
+  let i = 0;
+  for (let schema of schemas) {
+    await writeFile(
+      `../../common/models/${modelNames[i]}.json`,
+      JSON.stringify(schemas[0][`time-track-development.${modelNames[i]}`], null, 2)
+    );
+    i++;
+  }
+
+
   // Expose models via REST API
   const configJson = await readFile('../model-config.json', 'utf-8');
   console.log('MODEL CONFIG', configJson);
   const config = JSON.parse(configJson);
-  config.authority = {dataSource: DATASOURCE_NAME, public: true};
-  config.crew = {dataSource: DATASOURCE_NAME, public: true};
+
+  for(let modelName of modelNames){
+    config[_.chain(modelName).camelCase().upperFirst()] = { dataSource: DATASOURCE_NAME, public: true }
+  }
+  
   await writeFile(
     '../model-config.json',
     JSON.stringify(config, null, 2)
   );
 }
+
