68f55079fc59691effe43774bce607238e0aac6c
Joshua Wootonn <joshuawootonn@gmail.com>
2018-06-26 22:10:26 -0500

    Finally finished login code after logic

--- desktop/src/store/User/actions.js
+++ desktop/src/store/User/actions.js
@@ -3,14 +3,20 @@ import * as endpoint from './endpoints';
 
 
 export const login = (username, password) => {
-  
+
   return dispatch => {
     dispatch({ type: types.USER_LOGIN_REQUEST });
-    
+
     return endpoint.login(username, password).then(
-      response =>  dispatch({ type: types.USER_LOGIN_SUCCESS, payload: response.data }),
-      error    =>  dispatch({ type: types.USER_LOGIN_FAILURE, payload: error })
-    )  
-    
+      response => {
+        dispatch({ type: types.USER_LOGIN_SUCCESS, payload: response.data })
+        return Promise.resolve();
+      },
+      error => {
+        dispatch({ type: types.USER_LOGIN_FAILURE, payload: error })
+        return Promise.reject(error);
+      }
+    )
+
   };
 };
