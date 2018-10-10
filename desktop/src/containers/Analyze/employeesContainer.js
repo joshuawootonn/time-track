8cb97418ea8a8036c89639c90997ecc23b8d01eb
Joshua Wootonn <jose56wonton@gmail.com>
2018-10-09 23:30:20 -0500

    hook up employee container with actions/selectors for getting employee

--- desktop/src/containers/Analyze/employeesContainer.js
+++ desktop/src/containers/Analyze/employeesContainer.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+import { employeeActions } from 'store/actions';
+import {employeeSelectors } from 'store/selectors';
 
 class EmployeeContainer extends Component {
-  render () {
+  componentDidMount = () => {
+    this.props.getEmployees();
+  }
+  render() {
+    console.log(this.props.employees[0].firstName);
     return (
       <div>
         employee
+        {this.props.employees.map((ele) => {
+          return <div>{ele.firstName}</div>
+        })}
       </div>
     )
   }
 }
 
-export default EmployeeContainer
+const mapStateToProps = (state) => {
+  return {
+    employees: employeeSelectors.getAllEmployees(state)
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getEmployees: () => {
+      return dispatch(employeeActions.getEmployees())
+    }
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(EmployeeContainer);
