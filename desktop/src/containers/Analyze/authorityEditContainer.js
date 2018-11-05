3da37bd54d6602bb7c1624860049d362ba8decfd
Joshua Wootonn <jose56wonton@gmail.com>
2018-11-04 21:28:16 -0600

    infastructure around authority edit

--- desktop/src/containers/Analyze/authorityEditContainer.js
+++ desktop/src/containers/Analyze/authorityEditContainer.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import { Grid } from '@material-ui/core';
+
+import AuthorityIndexContainer from 'containers/Analyze/authorityIndexContainer';
+import AuthorityDetailContainer from 'containers/Analyze/authorityDetailContainer';
 
 class AuthorityEditContainer extends Component {
   render () {
     return (
-      <div>
-        authority edit
-      </div>
-    )
+      <Grid container>
+        <Grid item xs={7}>
+          <AuthorityIndexContainer />
+        </Grid>
+        <Grid item xs={5}>
+          <AuthorityDetailContainer />
+        </Grid>
+      </Grid>
+    );
   }
 }
 
-export default AuthorityEditContainer
+export default AuthorityEditContainer;
---
+++ desktop/src/containers/Analyze/authorityIndexContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AuthorityIndexContainer extends Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default AuthorityIndexContainer;
