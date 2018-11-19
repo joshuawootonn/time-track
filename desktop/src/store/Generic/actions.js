7247bb445e15806da67c86ff6a0f4dc56c7e6b7d
Joshua Wootonn <joshuawootonn@gmail.com>
2018-11-19 10:57:56 -0600

    implement 5 generic CRUD routes

--- desktop/src/store/Generic/actions.js
+++ desktop/src/store/Generic/actions.js
@@ -2,11 +2,11 @@ import { normalize } from 'normalizr';
 import * as endpoints from 'store/endpoints';
 import * as schemas from 'store/schemas';
 
-export const get = domain => {
+export const get = (domain,id) => {
   return async dispatch => {    
     dispatch({ type: `get_${domain.singular}_request` });
     try {
-      const response = await endpoints[`${domain.singular}Endpoints`].get();
+      const response = await endpoints[`${domain.singular}Endpoints`].default.get(id);
       const payload = normalize({ [domain.plural]: [response.data] }, schemas[`${domain.singular}Array`]);
       return dispatch({ type: `get_${domain.singular}_success`, payload });
     } catch (e) {
@@ -25,6 +25,52 @@ export const getAll = domain =>{
     } catch (e) {
       console.log(e);
       return dispatch({ type: `get_${domain.plural}_failure`,payload: e });
+    }
+  };
+};
+export const put = (domain,object) =>{
+  return async dispatch => {    
+    dispatch({ type: `put_${domain.singular}_request` });
+    try {      
+      const response = await endpoints[`${domain.singular}Endpoints`].default.put(object);
+      const payload = normalize({ [domain.plural]: response.data }, schemas[`${domain.singular}Array`]);
+      return dispatch({ type: `put_${domain.singular}_success`, payload });
+    } catch (e) {
+      console.log(e);
+      return dispatch({ type: `put_${domain.singular}_failure`,payload: e });
+    }
+  };
+};
+export const post = (domain, object) => {
+  return async dispatch => {    
+    dispatch({ type: `post_${domain.singular}_request` });
+    try {      
+      const response = await endpoints[`${domain.singular}Endpoints`].default.post(object);
+      const payload = normalize({ [domain.plural]: response.data }, schemas[`${domain.singular}Array`]);
+      return dispatch({ type: `post_${domain.singular}_success`, payload });
+    } catch (e) {
+      console.log(e);
+      return dispatch({ type: `post_${domain.singular}_failure`,payload: e });
+    }
+  };
+};
+export const delet = (domain,id) => {
+  return async dispatch => {    
+    dispatch({ type: `delete_${domain.singular}_request` });
+    try {
+      await endpoints[`${domain.singular}Endpoints`].default.delet(id);
+      const deleted ={
+        entities: {
+          [domain.plural] : [id]
+        },
+        result: {
+          [domain.plural] : [id]
+        }
+      };
+      return dispatch({ type: `delete_${domain.singular}_success`, deleted });
+    } catch (e) {
+      console.log(e);
+      return dispatch({ type: `delete_${domain.singular}_failure`,payload: e });
     }
   };
 };
