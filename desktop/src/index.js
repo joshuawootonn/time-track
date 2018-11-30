26e44a431d8dbbfdf68456cdaa8ee1a9d8d83e28
Joshua Wootonn <jose56wonton@gmail.com>
2018-11-30 17:17:36 -0600

    refactor to pull store into it's own file for easier testing

--- desktop/src/index.js
+++ desktop/src/index.js
@@ -3,19 +3,10 @@ import ReactDOM from 'react-dom';
 import App from 'app';
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 
-import reducer from 'store/reducers';
+import store from './store'
 import './index.css';
-const middleware = [thunk];
-export const store = createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(...middleware)),
-);
 
 
 const theme = createMuiTheme({
--- desktop/src/setupTests.js
+++ desktop/src/setupTests.js
@@ -1,7 +1,10 @@
 import 'jest-enzyme';
-
+import jsdom from 'jsdom'
 const Enzyme = require('enzyme');
 
 const Adapter = require('enzyme-adapter-react-16');
 // This sets up the adapter to be used by Enzyme
 Enzyme.configure({ adapter: new Adapter() });
+
+// global.window = new jsdom.JSDOM().window;
+// global.document = window.document;
