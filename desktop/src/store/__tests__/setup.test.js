08a074d9c2b9f414cf378a5f9770662c203f1711
Joshua Wootonn <jose56wonton@gmail.com>
2018-12-10 15:23:59 -0600

    test coverage of store setup

---
+++ desktop/src/store/__tests__/setup.test.js
@@ -0,0 +1,11 @@
+import * as actions from 'store/actions'
+import * as selectors from 'store/selectors';
+import reducers from 'store/reducers';
+
+describe('Store setup', () => { 
+  it('should all be defined', () => {
+    expect(actions).toBeDefined();
+    expect(selectors).toBeDefined();
+    expect(reducers).toBeDefined();
+  });
+});
