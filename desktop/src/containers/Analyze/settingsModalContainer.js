6265085bc877d2cf89a3e32a80c542ad46d2ffa5
Joshua Wootonn <jose56wonton@gmail.com>
2018-12-29 16:00:38 -0600

    update settings styling

--- desktop/src/containers/Analyze/settingsModalContainer.js
+++ desktop/src/containers/Analyze/settingsModalContainer.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
-import { Typography, List, ListItem, ListItemText, Grid } from '@material-ui/core';
-
+import { Typography, List, ListItem, ListItemText, Grid, Collapse, ListItemIcon } from '@material-ui/core';
+import { Category, Apps, Person, Group  } from '@material-ui/icons';
 import Modal from 'components/floaters/Modal';
 
 import AuthorityIndexContainer from 'containers/Analyze/authorityIndexContainer';
@@ -16,9 +16,10 @@ import SubcategoryIndexContainer from 'containers/Analyze/subcategoryIndexContai
 import SubcategoryDetailContainer from 'containers/Analyze/subcategoryDetailContainer';
 
 
-const styles = () => ({
+const styles = theme => ({
   modal: {
     width: '90%',
+    maxWidth: '1200px',
     height: '80vh',
     maxHeight: '90%'
   },
@@ -35,14 +36,29 @@ const styles = () => ({
   },
   detail: {
     minWidth: '400px'
+  },
+  nested: {
+    paddingLeft: theme.spacing.unit * 4
   }
 });
 
-const settings = ['Authority', 'Crew', 'Category', 'Subcategory'];
+const settings = [{
+  label: 'Authority',
+  icon: Person
+},{
+  label: 'Crew',
+  icon: Group
+},{
+  label: 'Category',
+  icon: Category
+},{
+  label: 'Subcategory',
+  icon: Apps
+}];
 
 export class SettingsModal extends Component {
   state = {
-    currentMenu: 0
+    currentMenu: 0    
   }
   menuSelect = num => () => {
     this.setState({
@@ -55,19 +71,24 @@ export class SettingsModal extends Component {
     const { currentMenu } = this.state;
     return (
       <Modal className={classes.modal} open={open} toggle={toggleModal}>
-        {/* <Typography variant="h3">Settings</Typography> */}
+        
         <Grid container className={classes.gridContainer}  spacing={8}>
           <Grid item xs={2}>
-            <div className={classes.listWrapper}>
-              <List>
+            <Typography variant="h4">Settings</Typography>
+            <div className={classes.listWrapper}>            
+                
+              <List >
                 {settings.map((setting, i) => {
                   return (
-                    <ListItem key={i}>
-                      <ListItemText onClick={this.menuSelect(i)} primary={setting} />
+                    <ListItem button key={i} selected={this.state.currentMenu === i} onClick={this.menuSelect(i)}className={classes.nested}>
+                      <ListItemIcon>
+                        <setting.icon />
+                      </ListItemIcon>
+                      <ListItemText primary={setting.label} />
                     </ListItem>
                   );
                 })}
-              </List>
+              </List>        
             </div>
           </Grid>
           
