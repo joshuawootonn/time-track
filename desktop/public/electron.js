fe91649b186ef4629534963a8f6d4f00aa1cb736
Joshua Wootonn <jose56wonton@gmail.com>
2019-03-23 19:51:29 -0500

    package.json updated with versions of electron packages that are compatible

--- desktop/public/electron.js
+++ desktop/public/electron.js
@@ -6,7 +6,6 @@ const ipcMain = electron.ipcMain;
 const url = require('url');
 const path = require('path');
 const settings = require('electron-settings');
-const log = require("electron-log")
 
 const IPCConstants = {
   SET_CRED: 'set_cred',
@@ -52,9 +51,10 @@ function createWindow() {
     mainWindow = null;
   });
 
+  const log = require("electron-log")
   log.transports.file.level = "debug"
   autoUpdater.logger = log
-  autoUpdater.checkForUpdatesAndNotify();
+  autoUpdater.checkForUpdatesAndNotify()
 }
 
 // This method will be called when Electron has finished
@@ -180,37 +180,37 @@ ipcMain.on(IPCConstants.CREATE_EXPORT, (event, arg) => {
  * Auto Update
  */
 
-const sendStatusToWindow = text => {
-  if (mainWindow) {
-    mainWindow.webContents.send('message', text);
-  }
-};
+// const sendStatusToWindow = text => {
+//   if (mainWindow) {
+//     mainWindow.webContents.send('message', text);
+//   }
+// };
 
 
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
-});
-autoUpdater.on('update-available', info => {
-  sendStatusToWindow('Update available.');
-});
-autoUpdater.on('update-not-available', info => {
-  sendStatusToWindow('Update not available.');
-});
-autoUpdater.on('error', err => {
-  sendStatusToWindow(`Error in auto-updater: ${err.toString()}`);
-});
-autoUpdater.on('download-progress', progressObj => {
-  sendStatusToWindow(
-    `Download speed: ${progressObj.bytesPerSecond} - Downloaded ${progressObj.percent}% (${progressObj.transferred} + '/' + ${progressObj.total} + )`
-  );
-});
-autoUpdater.on('update-downloaded', info => {
-  sendStatusToWindow('Update downloaded; will install now');
-});
+// autoUpdater.on('checking-for-update', () => {
+//   sendStatusToWindow('Checking for update...');
+// });
+// autoUpdater.on('update-available', info => {
+//   sendStatusToWindow('Update available.');
+// });
+// autoUpdater.on('update-not-available', info => {
+//   sendStatusToWindow('Update not available.');
+// });
+// autoUpdater.on('error', err => {
+//   sendStatusToWindow(`Error in auto-updater: ${err.toString()}`);
+// });
+// autoUpdater.on('download-progress', progressObj => {
+//   sendStatusToWindow(
+//     `Download speed: ${progressObj.bytesPerSecond} - Downloaded ${progressObj.percent}% (${progressObj.transferred} + '/' + ${progressObj.total} + )`
+//   );
+// });
+// autoUpdater.on('update-downloaded', info => {
+//   sendStatusToWindow('Update downloaded; will install now');
+// });
 
-autoUpdater.on('update-downloaded', info => {
-  // Wait 5 seconds, then quit and install
-  // In your application, you don't need to wait 500 ms.
-  // You could call autoUpdater.quitAndInstall(); immediately
-  autoUpdater.quitAndInstall();
-});
+// autoUpdater.on('update-downloaded', info => {
+//   // Wait 5 seconds, then quit and install
+//   // In your application, you don't need to wait 500 ms.
+//   // You could call autoUpdater.quitAndInstall(); immediately
+//   autoUpdater.quitAndInstall();
+// });
