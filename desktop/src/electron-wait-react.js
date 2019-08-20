b5dae7beaa61a97de7d3d1e748a038d2bbd6a87d
Joshua Wootonn <jose56wonton@gmail.com>
2019-08-19 21:05:33 -0500

    more attempts

--- desktop/src/electron-wait-react.js
+++ desktop/src/electron-wait-react.js
@@ -1,24 +1,20 @@
-const net = require(`net`);
-const port = process.env.PORT ? process.env.PORT - 100 : 3000;
-
-process.env.ELECTRON_START_URL = `http://localhost:${port}`;
-
-const client = new net.Socket();
-
-let startedElectron = false;
-const tryConnection = () =>
-  client.connect({ port: port }, () => {
-    client.end();
-    if (!startedElectron) {
-      // console.log('starting electron');
-      startedElectron = true;
-      const exec = require(`child_process`).exec;
-      exec(`npm run electron`);
-    }
-  });
-
+var net = require("net");
+var port = process.env.PORT ? Number(process.env.PORT) - 100 : 3000;
+process.env.ELECTRON_START_URL = "http://localhost:" + port;
+var client = new net.Socket();
+var startedElectron = false;
+var tryConnection = function () {
+    return client.connect({ port: port }, function () {
+        client.end();
+        if (!startedElectron) {
+            // console.log('starting electron');
+            startedElectron = true;
+            var exec = require("child_process").exec;
+            exec("npm run electron");
+        }
+    });
+};
 tryConnection();
-
-client.on(`error`, () => {
-  setTimeout(tryConnection, 1000);
+client.on("error", function () {
+    setTimeout(tryConnection, 1000);
 });
