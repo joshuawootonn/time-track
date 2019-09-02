dfae1e4c6c110c46b628b285655b821276da5b7c
Joshua Wootonn <jose56wonton@gmail.com>
2019-09-01 20:09:20 -0500

    part of the way through clockout

--- api/common/models/employee.js
+++ api/common/models/employee.js
@@ -1,3 +1,6 @@
+var loopback = require("loopback");
+const moment = require("moment");
+
 var baseError = {
   name: "Error",
   status: 400,
@@ -5,25 +8,30 @@ var baseError = {
 };
 
 module.exports = Employee => {
-  Employee.clockin = async (employeeId, cb) => {
-    var app = require("../../server/server");
-    const Shift = app.models.Shift;
+  var app = require("../../server/server");
+  const Shift = app.models.Shift;
+  const Activity = app.models.Activity;
 
-    if(!employeeId){ 
-      return cb({...baseError, message: "employeeId is a required body value"});
+  Employee.clockin = async (employeeId, cb) => {
+    if (!employeeId) {
+      return cb({
+        ...baseError,
+        message: "employeeId is a required body value"
+      });
     }
 
     const lastShift = await Shift.findOne({
+      order: { id: "DESC" },
       where: { employeeId: employeeId }
     });
     const employee = await Employee.findOne({ where: { id: employeeId } });
 
     if (lastShift && !lastShift.clockOutDate) {
-      return cb({...baseError, message: "Employee already has open shift"});
-    } else if(!employee){
-      return cb({...baseError, message: "Employee doesn't exist"}); 
-    }else if ( employee.isWorking) {
-      return cb({...baseError, message: "Employee isWorking"});
+      return cb({ ...baseError, message: "Employee already has open shift" });
+    } else if (!employee) {
+      return cb({ ...baseError, message: "Employee doesn't exist" });
+    } else if (employee.isWorking) {
+      return cb({ ...baseError, message: "Employee is Working" });
     }
 
     // check that employee !isWorking and doesn't have an open shift
@@ -37,8 +45,57 @@ module.exports = Employee => {
     cb(null, `${employee.firstName} ${employee.lastName} is clocked in`);
   };
 
+  Employee.clockout = async (employeeId, shift, activities, cb) => {
+    if (!employeeId) {
+      return cb({
+        ...baseError,
+        message: "employeeId is a required body value"
+      });
+    }
+
+    console.log(employeeId, shift, activities);
+    const lastShift = await Shift.findOne({
+      order: { id: "DESC" },
+      where: { employeeId: employeeId }
+    });
+    const employee = await Employee.findOne({ where: { id: employeeId } });
+
+    if (lastShift && lastShift.clockOutDate) {
+      return cb({ ...baseError, message: "Employee has no open shift" });
+    } else if (!employee) {
+      return cb({ ...baseError, message: "Employee doesn't exist" });
+    } else if (!employee.isWorking) {
+      return cb({ ...baseError, message: "Employee is not Working" });
+    }
+    // activities.forEach( async activity => {
+    //   const a = Activity.create({
+    //     ...activity
+    //   })
+    //   console.log('Created activity: ', a);
+    // });
+
+    // // check that employee !isWorking and doesn't have an open shift
+    // const s = await lastShift.updateAttributes({
+    //   clockOutDate: moment().toString(),
+    //   length: moment.duration(moment().diff(moment(lastShift.clockInDate))),
+    //   lunch: shift.lunch,
+    // });
+    // console.log('Updated shift: ', s);
+    // const e = await employee.updateAttribute("isWorking", false);
+    // console.log('Updated employee: ', e);
+    cb(null, `${employee.firstName} ${employee.lastName} is clocked in`);
+  };
+
   Employee.remoteMethod("clockin", {
     accepts: { arg: "employeeId", type: "number" },
+    returns: { arg: "msg", type: "string" }
+  });
+  Employee.remoteMethod("clockout", {
+    accepts: [
+      { arg: "employeeId", type: "number" },
+      { arg: "shift", type: "number" },
+      { arg: "activities", type: "number" }
+    ],
     returns: { arg: "msg", type: "string" }
   });
 };
