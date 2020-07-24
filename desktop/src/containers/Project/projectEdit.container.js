2679cc42715902de84a018308c085b01dc2a9feb
joshuawootonn <joshuawootonn@gmail.com>
2020-07-23 23:02:48 -0500

    feat: recent shifts button in project section

--- desktop/src/containers/Project/projectEdit.container.js
+++ desktop/src/containers/Project/projectEdit.container.js
@@ -3,16 +3,18 @@ import moment from 'moment';
 import { projectEditValidation } from 'constants/formValidation';
 import ProjectEdit from 'components/forms/Project/projectEdit';
 import { Formik } from 'formik';
-import { useDispatch } from 'react-redux';
-import { projectActions } from 'store/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { analyzeActions, projectActions } from 'store/actions';
 import axios from 'helpers/axios';
 import Progress from 'components/helpers/Progress';
+import domain from 'constants/domains';
 
-const ProjectEditContainer = ({ selected, tasks }) => {
+const ProjectEditContainer = ({ selected, tasks, goToTab }) => {
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(true);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [newSelected, setNewSelected] = useState({});
+  const shiftFilters = useSelector(state => state.analyze.shiftFilters);
 
   useEffect(() => {
     setIsLoading(true);
@@ -50,6 +52,24 @@ const ProjectEditContainer = ({ selected, tasks }) => {
 
   const updateProject = project =>
     dispatch(projectActions.updateProject(project));
+
+  const onGoToShifts = () => {
+    dispatch(
+      analyzeActions.updateFilter(domain.SHIFT, {
+        ...shiftFilters,
+        projectId: selected.id,
+        startTime: moment
+          .utc()
+          .subtract(2, 'months')
+          .format(`MM-DD-YY HH:mm:ss`),
+        endTime: moment
+          .utc()
+          .add(1, 'day')
+          .format(`MM-DD-YY HH:mm:ss`)
+      })
+    );
+    goToTab(3);
+  };
 
   if (isSubmitting) {
     return (
@@ -105,6 +125,7 @@ const ProjectEditContainer = ({ selected, tasks }) => {
         <ProjectEdit
           tasks={tasks}
           removeProject={removeProject}
+          goToShifts={onGoToShifts}
           {...formikProps}
         />
       )}
