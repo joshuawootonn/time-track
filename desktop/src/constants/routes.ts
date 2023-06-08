79154362a261f92a068c6e20806855e1ff19b7d1
odin5on <108775964+odin5on@users.noreply.github.com>
2023-06-08 11:08:12 -0500

    feat: add routes for foreman accountAction view to load with clock-in and back arrow

--- desktop/src/constants/routes.ts
+++ desktop/src/constants/routes.ts
@@ -1,10 +1,11 @@
 export const ROOT = `/`;
 
-export type AUTH_LEVELS = 'employee' | 'admin' | 'manager';
+export type AUTH_LEVELS = 'employee' | 'admin' | 'manager' | 'foreman';
 
 export const EMPLOYEE = `employee`;
 export const ADMIN = `admin`;
 export const MANAGER = `manager`;
+export const FOREMAN = 'foreman';
 
 export const CLOCKOUT = `clockout`;
 export const EXPORT = `export`;
