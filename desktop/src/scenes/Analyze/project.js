1f32e2a6dffdb4d8e10b59a9b7ba76878e22bb0d
odin5on <108775964+odin5on@users.noreply.github.com>
2023-06-15 10:39:58 -0500

    wip: project selector rendering in projects page

--- desktop/src/scenes/Analyze/project.js
+++ desktop/src/scenes/Analyze/project.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
+import { AppBar, IconButton, Toolbar, Tooltip, Grid } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import { ArrowBack } from '@material-ui/icons';
+
+import TypeableSelect from 'components/inputs/TypeableSelect';
+import PropTypes from 'prop-types';
+
+import { Field, Form, Formik } from 'formik';
+
+import { projectActions } from 'store/actions';
+import { projectSelectors } from 'store/selectors';
 
 const styles = {
   root: {
@@ -28,11 +38,98 @@ const styles = {
 };
 
 export class Project extends Component {
+  back = () => {
+    this.props.history.push(`/`);
+  };
+
   render() {
+    const { classes, projects } = this.props;
     return (
-    <p>hello world</p>
+      <div className={classes.root}>
+        <AppBar position="static" elevation={0}>
+          <Toolbar className={classes.tool}>
+            <Grid
+              container
+              direction="row"
+              alignItems="center"
+              spacing={1}
+            >
+              <Grid item xs={10}>
+                <Formik>
+                  <Form>
+                    <Field
+                      name="Project select"
+                      component={TypeableSelect}
+                      type="name"
+                      items={projects}
+                      fullWidth
+                      label="Project"
+                      id={`${ANALYZE_SHIFT_FULL_SHIFT_PROJECT_FIELD_ID}`}
+                      className={classes.field}
+                    />
+                  </Form>
+                </Formik>
+              </Grid>
+              <Grid item xs={1}>
+                <Tooltip title="Go Back" placement="bottom">
+                  <IconButton color="inherit" onClick={this.back}>
+                    <ArrowBack />
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+        {/* <button onClick={console.log(this.props.getAllProjects())} /> */}
+        {/* <Form name="hi">
+          <Field
+            name="hi"
+            component={TypeableSelect}
+            type="name"
+            items={this.props.getAllProjects}
+            fullWidth
+            label="Project"
+            className={classes.field}
+          />
+        </Form> */}
+        {/* <div>
+          <Formik
+            render={formikProps => {
+              return (
+                <Field name="firstName" placeholder="Jane" {...formikProps} />
+              );
+            }}
+          />
+        </div> */}
+      </div>
     );
   }
 }
 
-export default withRouter(withStyles(styles)(Project));
+export const ANALYZE_SHIFT_FULL_SHIFT_PROJECT_FIELD_ID = `analyze_shift_full_shift_project_field`;
+
+Project.prototypes = {
+  getAllProjects: PropTypes.func.isRequired,
+};
+
+/* istanbul ignore next */
+const mapStateToProps = state => {
+  return {
+    projects: projectSelectors.getActiveProjects(state),
+  };
+};
+
+/* istanbul ignore next */
+const mapDispatchToProps = dispatch => {
+  return {
+    getAllProjects: () => {
+      return dispatch(projectActions.getAllProjects());
+    },
+  };
+};
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(withStyles(styles)(Project)));
