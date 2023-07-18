6c7052089c9c73e8e14c1f7a26577d88ac7b29ff
Josh Wootonn <joshuawootonn@gmail.com>
2023-07-18 10:52:00 -0500

    feat: remove unuse deps/upgrade prettier

--- api/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  parser: "babel-eslint",
-  extends: ['loopback', 'prettier'], // extending recommended config and config derived from eslint config prettier
-  plugins: ['jest'], // activating esling plugin prettier (  fix stuff)  
-  "env": {
-    "jest/globals": true
-  }
-}; 
