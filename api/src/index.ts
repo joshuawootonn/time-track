3347b689a7ab88e0625c56d7c4194a86eac46aae
Joshua Wootonn <joshuawootonn@gmail.com>
2024-01-21 13:26:31 -0500

    feat: migrate express middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,39 +1,35 @@
-import {ApplicationConfig, ApiApplication} from './application';
+import { ApplicationConfig } from './application'
+import { ExpressServer } from './express-server'
 
-export * from './application';
+export * from './application'
 
 export async function main(options: ApplicationConfig = {}) {
-  const app = new ApiApplication(options);
-  await app.boot();
-  await app.start();
+  const server = new ExpressServer(options)
+  await server.boot()
+  const address = await server.start()
 
-  const url = app.restServer.url;
-  console.log(`Server is running at ${url}`);
-  console.log(`Try ${url}/ping`);
-
-  return app;
+  console.log(`Server is running at ${address}`)
 }
 
 if (require.main === module) {
-  // Run the application
-  const config = {
+  const config: ApplicationConfig = {
     rest: {
-      port: 4000,
-      host: process.env.HOST,
-      // The `gracePeriodForClose` provides a graceful close for http/https
-      // servers with keep-alive clients. The default value is `Infinity`
-      // (don't force-close). If you want to immediately destroy all sockets
-      // upon stop, set its value to `0`.
-      // See https://www.npmjs.com/package/stoppable
+      port: process.env.PORT ?? 4000,
+      host: process.env.HOST ?? 'localhost',
       gracePeriodForClose: 5000, // 5 seconds
       openApiSpec: {
         // useful when used with OpenAPI-to-GraphQL to locate your application
         setServersFromRequest: true,
       },
+      // Use the LB4 application as a route. It should not be listening.
+      listenOnStart: false,
     },
-  };
-  main(config).catch(err => {
-    console.error('Cannot start the application.', err);
-    process.exit(1);
-  });
+    lb3app: {
+      mode: 'fullApp',
+    },
+  }
+  main(config).catch((err) => {
+    console.error('Cannot start the application.', err)
+    process.exit(1)
+  })
 }
