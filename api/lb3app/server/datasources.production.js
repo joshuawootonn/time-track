d8d23400f162c7b03184dc643dde495eb4000ffb
Joshua Wootonn <joshuawootonn@gmail.com>
2024-01-25 06:57:54 -0500

    feat: upgrade datasources to upgraded mysql connector
    This puts us on mysql2 and allows us to use connection strings, and allows us to not disable new auth mode in our dbs

--- api/lb3app/server/datasources.production.js
+++ api/lb3app/server/datasources.production.js
@@ -1,8 +1,11 @@
+console.log(
+  'Loopback 3 loaded with connection string: ',
+  process.env.SQL_CONNECTION_STRING,
+)
 module.exports = {
   db: {
     url: process.env.SQL_CONNECTION_STRING,
     name: 'db',
-    user: process.env.SQL_USER,
     connector: 'mysql',
   },
 }
