ef74e74315fab62b261257160b2baec7bb6cf248
Tim Goloshchapov <tim.goloschapov@gmail.com>
2026-04-09 13:40:55 -0500

    lunchbox final

--- desktop/src/components/inputs/Time/styles.jsx
+++ desktop/src/components/inputs/Time/styles.jsx
@@ -4,23 +4,38 @@ const styles = () => ({
     flexDirection: `column`,
     width: `100%`,
   },
+  autoWidth: {
+    width: 'auto',
+  },
   vertical: {
     flexDirection: `column`,
   },
   horizontal: {
     flexDirection: `row`,
   },
+  selectWithSuffix: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+    '& + &': {
+      marginLeft: '10px',
+    },
+  },
+  suffix: {
+    marginLeft: '8px',
+    whiteSpace: 'nowrap',
+  },
   helper: {
-    margin: '0 8px'
+    margin: '0 8px',
   },
   '@media (max-width: 500px)': {
     hours: {
       marginRight: '8px !important',
     },
     helper: {
-      margin: '0 0px'
-    }
-  }
+      margin: '0 0px',
+    },
+  },
 })
 
 export default styles
